5aa1ec595428282a6b8089404de0706e8994047b
fabiogalo <fabio.galo.dominguez@hotmail.com>
2013-10-19 13:34:44 -0500

    Primer ejercicio
    Primer ejercicio

---
+++ js/API/eventos.js
@@ -0,0 +1,35 @@
+// eventos
+/*jQuery(document).ready(function(){	
+	var boton = document.getElementById('regEnv');
+	boton.addEventListener("click", function (){
+		alert();
+	}, false);
+});*/
+
+$(function(){
+	document.addEventListener("deviceready", function(){
+		$('#regEnv').click (function (){
+			var nom = $('#regNom').val();
+			var mail = $('#regEma').val();
+			var tel = $('#regTel').val();
+			
+			if(nom != '' && mail != '' && tel != ''){
+				navigator.notification.confirm(nom+'\n' + mail+'\n' + tel,
+				function(btn){
+					switch(btn){
+						case 1:
+							navigator.notification.beep(2);
+							break;
+
+						case 2:
+							navigator.notification.vibrate(2000);
+							break;
+					}
+				}, 'Datos de Registro', 'Beep, Vibrar, Cancelar');
+			}
+			else {
+				navigator.notification.alert('Todos los campos seon requeridos', null, 'Error', 'ACEPTAR');
+			}
+		});
+	}, false);
+});
